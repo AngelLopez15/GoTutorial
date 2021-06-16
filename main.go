80bc435d3afe00ac4d93083314cfae0c946a005f
Angel Lopez <a.o.lopez.cruz@gmail.com>
2021-06-15 21:58:51 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+// fmt es el paquete estandar de GO que contiene varias funcionalidades. Entre ellas imprimir en pantalla
+import "fmt"
+
+func main() {
+	fmt.Println("Hola, mundo, desde GO")
+}
+
+// Para correr un programa debemos teclear el comando
+// go run nombreArchivo.go
